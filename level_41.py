8c04cbf95e4127486e2ec63b3a9182507a89e799
Nick Schneeberger <nick.schneeberger@icloud.com>
2025-02-25 11:45:36 +0100

    do somethin

--- level_41.py
+++ level_41.py
@@ -6,7 +6,7 @@ def main():
     proxy = "username:password@proxy_host:proxy_port"
 
     with SB(uc=True, headless=True, proxy=proxy) as sb:
-        sb.driver.uc_open_with_reconnect("https://www.coop.ch/de/weine/alle-weine/c/m_2508?q=%3Arelevance&sort=relevance&pageSize=60&page=1#1000666018", reconnect_time=10)
+        sb.driver.uc_open_with_reconnect("https://www.digezz.ch", reconnect_time=10)
         
         print(sb.driver.title)
         print(sb.driver.page_source)
